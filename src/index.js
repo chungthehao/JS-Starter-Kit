9563d70ca2669b9f112db858b96c0ec253132a59
Henry Chung <chungthehao@gmail.com>
2022-09-27 16:35:57 +0700

    Setup ESLint config file, Use ESlint Watch, some exceptions

--- src/index.js
+++ src/index.js
@@ -4,5 +4,4 @@ import "./index.css";
 import numeral from "numeral";
 
 const courseValue = numeral(1000).format("$0,0.00");
-debugger;
-console.log(`I would get paid for ${courseValue} per hour!`);
+console.log(`I would get paid for ${courseValue} per hour!`); // eslint-disable-line no-console
