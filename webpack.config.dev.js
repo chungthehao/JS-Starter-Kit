02cd86fc0687b859a176d15207d0a03d37f079e3
Henry Chung <chungthehao@gmail.com>
2022-09-27 08:40:45 +0700

    Config Webpack

---
+++ webpack.config.dev.js
@@ -0,0 +1,19 @@
+import path from "path";
+
+export default {
+  mode: "development",
+  devtool: "eval-source-map",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "src"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  plugins: [],
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: ["babel_loader"] },
+      { test: /\.css$/, use: ["style_loader", "css-loader"] },
+    ],
+  },
+};
