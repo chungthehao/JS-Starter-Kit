9e552d1d8f57552ee671fdd8b8fd56d69ec59657
Henry Chung <chungthehao@gmail.com>
2022-09-28 20:05:52 +0700

    Test should fail

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import "./index.css";
 import numeral from "numeral";
 
 const courseValue = numeral(1000).format("$0,0.00");
-console.log(`I would get paid for ${courseValue} per hour!?`); // eslint-disable-line no-console
+console.log(`I would get paid for ${courseValue} per hour!`); // eslint-disable-line no-console
