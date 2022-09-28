9685d684f998fd50fad727d1fb69aa3bc18197e5
Henry Chung <chungthehao@gmail.com>
2022-09-28 08:21:09 +0700

    DOM Testing

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,23 @@
 import { expect } from "chai";
+import { JSDOM } from "jsdom";
+import fs from "fs";
 
 describe("My first test", () => {
   it("should pass", () => {
     expect(true).to.equal(true);
+  });
+});
+
+describe("index.html", () => {
+  it("should say welcome...", () => {
+    const index = fs.readFileSync("./src/index.html");
+    const dom = new JSDOM(index);
+
+    const h2 = dom.window.document.getElementsByTagName("h2")[0];
+    expect(h2.innerHTML).to.equal(
+      "Welcome to Henry's JS Development Environment!"
+    );
+
+    dom.window.close();
   });
 });
